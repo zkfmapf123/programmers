cb5892328e452641c9cd27e71d38fff87f121639
zkfmapf123 <47292546+zkfmapf123@users.noreply.github.com>
2022-11-27 22:56:28 +0900

    2022-11-27

--- main.go
+++ main.go
@@ -2,43 +2,25 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 
 func main(){
-	fmt.Println(maxProfit([]int{7,1,5,3,6,4}))
-	fmt.Println(maxProfit([]int{7,6,4,3,1}))
-	fmt.Println(maxProfit([]int{2,4,1}))	
-	fmt.Println(maxProfit([]int{1,2}))	
-
-	// v := []int{7,1,5,3,6,4}
-	// fmt.Println(v)
-	// fmt.Println(v[0:5])
+	// fmt.Println(longestPalindrome("abccccdd"))
+	// fmt.Println(longestPalindrome("a"))
+	// fmt.Println(longestPalindrome("ccc"))
+	// fmt.Println(longestPalindrome("bananas"))
+	fmt.Println(longestPalindrome("civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"))
 }
 
-// DD
-func maxProfit(prices []int) int {
-	if prices == nil || len(prices) == 0 {
-		return 0
+func longestPalindrome(s string) int {
+
+	m := map[string]int{}
+
+	for _, v := range strings.Split(s, ""){
+
 	}
 
-	
-	sum := 0
-	buy := prices[0]
-	for i,sell := range prices {
-		if i == 0 {
-			continue
-		}
-
-		if buy > sell {
-			buy = sell
-			continue
-		}
-
-		if buy < sell && sum < sell-buy {
-			sum = sell-buy
-		}	
-	}
-
-	return sum
+	return result
 }
