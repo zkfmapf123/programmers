710b27bc041f07a2b760e921431f42d050bef5a2
zkfmapf123 <47292546+zkfmapf123@users.noreply.github.com>
2023-03-22 01:29:25 +0900

    2023-03-22

---
+++ Back_2309.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var (
+	w = bufio.NewWriter(os.Stdout)
+	r = bufio.NewReader(os.Stdin)
+)
+
+// 아홉명의 난쟁이
+// 난쟁이는 총 일곱명 === 100
+// 2명만 빼면 됨
+func main(){
+	w.Flush()
+	N := 9
+
+	arr, sum := []int{}, 0
+	
+	for i:=0; i<N; i++ {
+		b, _, _ := r.ReadLine()
+		str := string(b)
+		v, _ := strconv.Atoi(str)
+		arr = append(arr, v)
+		sum += v
+	}
+	
+	sort.Ints(arr)
+	for i:=0; i<len(arr)-1; i++ {
+		for j:=i+1; j<len(arr); j++ {
+			
+			if arr[i] + arr[j] == (sum - 100) {
+
+				for k:=0; k<len(arr); k++ {
+					if k == i || k ==j {
+						continue
+					}
+
+					fmt.Println(arr[k])
+				}
+				return	
+			}
+		}
+	}
+}
+
+func getN() int{
+	var N int
+	fmt.Fscanf(r, "%d\n", &N)
+	return N
+}
---
+++ 최대값구하기.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main(){
+
+	maxList := []int{}
+	arr := []int{62,3,40,17,5,8,26,99}
+
+	for i:=0; i<len(arr)-1; i++ {
+		
+		for j:=i+1; j<len(arr); j++ {
+			maxList = append(maxList, arr[i] + arr[j])
+		}
+	}
+
+	sort.Ints(maxList)
+	fmt.Println(maxList[len(maxList)-1])
+	
+}
