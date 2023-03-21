064d2c816ec1f4f5038e6ec01e56f0bb2ab24aa0
zkfmapf123 <47292546+zkfmapf123@users.noreply.github.com>
2023-03-22 00:55:51 +0900

    2023.3.22

---
+++ Back_1302.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+)
+
+var (
+	w = bufio.NewWriter(os.Stdout)
+	r = bufio.NewReader(os.Stdin)
+)
+
+func main(){
+	defer w.Flush()
+
+	N := getN()
+	
+	m := make(map[string]int)
+
+	// o(n)
+	for i:=0; i<N; i++ {
+		str, _, _ := r.ReadLine()	
+		value := string(str)
+
+		v, isOk := m[value]
+		if isOk {
+			v++
+			m[value] = v
+		}else{
+			m[value] = 1
+		}
+	}
+
+	// o(n)
+	max := -1
+	for _,v := range m {
+		if v > max {
+			max =v
+		}
+	}
+
+	// o(n)
+	book := []string{}
+	for k,v := range m {
+
+		if v == max {
+			book = append(book, k)
+		}
+	}
+
+	sort.Strings(book)
+	fmt.Println(book[0])
+}
+
+func getN() int{
+	var N int
+	fmt.Fscanf(r, "%d\n", &N)
+	return N
+}
+
